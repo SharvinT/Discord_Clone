f0b98bf6c921924739b02894455577aee4bc0a51
Sharvin <sharvintheju@gmail.com>
2020-10-25 20:09:30 +0530

    Fixed scrolling bug and width adjustment

--- src/Chat.js
+++ src/Chat.js
@@ -1,95 +1,97 @@
-import React from "react";
+import {
+  AddCircle,
+  CardGiftcard,
+  EmojiEmotions,
+  Gif,
+} from "@material-ui/icons";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import "./Chat.css";
 import ChatHeader from "./ChatHeader";
-import AddCircleIcon from "@material-ui/icons/AddCircle";
-import CradGiftcardIcon from "@material-ui/icons/CardGiftcard";
-import GifIcon from "@material-ui/icons/Gif";
-import EmojiEmoticonsIcon from "@material-ui/icons/EmojiEmotions";
-import Message from "./Message";
-import { useSelector } from "react-redux";
-import { selectUser } from "./features/userSlice";
 import { selectChannelId, selectChannelName } from "./features/appSlice";
-import { useState } from "react";
-import { useEffect } from "react";
+import { selectUser } from "./features/userSlice";
 import db from "./firebase";
+import Message from "./Message";
 import firebase from "firebase";
 
-const Chat = () => {
+function Chat() {
   const user = useSelector(selectUser);
   const channelId = useSelector(selectChannelId);
   const channelName = useSelector(selectChannelName);
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
-
   useEffect(() => {
     if (channelId) {
       db.collection("channels")
         .doc(channelId)
         .collection("messages")
-        .orderBy("timestamp", "desc")
-        .onSnapshot((snapshot) => {
-          setMessages(snapshot.docs.map((doc) => doc.data()));
-        });
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
     }
   }, [channelId]);
 
   const sendMessage = (e) => {
     e.preventDefault();
-
     db.collection("channels").doc(channelId).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       message: input,
       user: user,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
-
     setInput("");
   };
 
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(scrollToBottom, [messages]);
+
   return (
     <div className="chat">
+      {/* herder */}
       <ChatHeader channelName={channelName} />
-
+      {/* Messages */}
       <div className="chat__messages">
-        {messages.map((message) => {
-          console.log(message);
-        })}
         {messages.map((message) => (
           <Message
-            message={message.message}
             timestamp={message.timestamp}
+            message={message.message}
             user={message.user}
           />
         ))}
+        <div ref={messagesEndRef} />
       </div>
-
+      {/* textbox */}
       <div className="chat__input">
-        <AddCircleIcon fontSize="large" />
+        <AddCircle fontSize="large" />
         <form>
           <input
-            type="text"
-            disabled={!channelId}
             value={input}
             onChange={(e) => setInput(e.target.value)}
+            type="text"
+            disabled={!channelId}
             placeholder={`Message #${channelName}`}
           />
           <button
-            className="chat__inputButton"
-            onClick={sendMessage}
             disabled={!channelId}
+            className="chat__inputButton"
             type="submit"
+            onClick={sendMessage}
           >
-            Send Message
+            Submit
           </button>
         </form>
-
-        <div className="chat__inputIcon">
-          <CradGiftcardIcon fontSize="large" />
-          <GifIcon fontSize="large" />
-          <EmojiEmoticonsIcon fontSize="large" />
+        <div className="chat__inputIcons">
+          <CardGiftcard fontSize="large" />
+          <Gif fontSize="large" />
+          <EmojiEmotions fontSize="large" />
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Chat;
