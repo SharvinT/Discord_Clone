d32341137b3c9b8a8c5b1ee536e915f7d194a097
Sharvin <sharvintheju@gmail.com>
2020-12-06 00:12:31 +0530

    Updated the axios with Heroku link

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8002",
+  // baseURL: "http://localhost:8002",
+  baseURL: "https://heroku-discord-backend.herokuapp.com",
 });
 
 export default instance;
